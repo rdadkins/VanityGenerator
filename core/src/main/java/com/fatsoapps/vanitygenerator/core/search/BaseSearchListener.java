e8135c031a9d23add9de0c63c642571998bd5240
Rider Adkins <rideradkins@gmail.com>
2015-11-25 15:17:01 -0500

    Added a new parameter to onAddressFound and added more documentation.

--- core/src/main/java/com/fatsoapps/vanitygenerator/core/search/BaseSearchListener.java
+++ core/src/main/java/com/fatsoapps/vanitygenerator/core/search/BaseSearchListener.java
@@ -10,10 +10,13 @@ public interface BaseSearchListener {
 
     /**
      * Called when a Search thread finds a key.
-     * @param key - ECKey that contains a matched key
+     * @param key - ECKey that contains a matched key.
+     * @param amountGenerated - the amount of addresses generated to find this match.
      * @param isCompressed - Whether the query was compressed or not.
+     * @see com.fatsoapps.vanitygenerator.core.search.Search
+     * @see com.fatsoapps.vanitygenerator.core.search.PoolSearch
      */
-    void onAddressFound(ECKey key, boolean isCompressed);
+    void onAddressFound(ECKey key, long amountGenerated, boolean isCompressed);
 
     /**
      * Called when a Search thread has generated @burstGenerated amount of addresses. This is meant to let the user know
@@ -26,7 +29,7 @@ public interface BaseSearchListener {
     /**
      * Called when a Search thread is done OR has be killed / cancelled by the user. This can be called multiple times
      * if there is another thread running in parallel.
-     * @param totalGenerated
+     * @param totalGenerated - total amount of addresses generated across single / multi threads.
      */
     void onTaskCompleted(long totalGenerated);
 
