68b5c9ef32466c078cd8989398a2c3df69fb3545
Rider Adkins <rideradkins@gmail.com>
2015-11-25 15:47:37 -0500

    Minor changes to Search

--- core/src/main/java/com/fatsoapps/vanitygenerator/core/search/Search.java
+++ core/src/main/java/com/fatsoapps/vanitygenerator/core/search/Search.java
@@ -33,13 +33,15 @@ public class Search implements Runnable {
     }
 
     public void run() {
-        ECKey key = new ECKey();
-        long generated = 1;
+        ECKey key;
+        long generated = 0;
         while (!Thread.interrupted() && queries.size() > 0) {
+            key = new ECKey();
+            generated++;
             for (Query query: queries) {
                 if (query.matches(key, netParams)) {
                     if (listener != null) {
-                        listener.onAddressFound(key, query.isCompressed());
+                        listener.onAddressFound(key, generated, query.isCompressed());
                     }
                     if (!query.isFindUnlimited()) {
                         queries.remove(query);
@@ -50,9 +52,6 @@ public class Search implements Runnable {
             if (generated % updateAmount == 0 && listener != null) {
                 listener.updateBurstGenerated(generated, updateAmount);
             }
-            key = new ECKey();
-            generated++;
-
         }
         if (listener != null) {
             listener.onTaskCompleted(generated);
