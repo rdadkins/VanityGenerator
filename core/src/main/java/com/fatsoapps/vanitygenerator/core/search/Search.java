e0971dedbe803e72799aa1515858c717846e696b
Rider Adkins <rideradkins@gmail.com>
2015-11-27 14:43:44 -0500

    Updated Search to accept RegexQuery's in place of regular Query's

--- core/src/main/java/com/fatsoapps/vanitygenerator/core/search/Search.java
+++ core/src/main/java/com/fatsoapps/vanitygenerator/core/search/Search.java
@@ -2,14 +2,16 @@ package com.fatsoapps.vanitygenerator.core.search;
 
 import com.fatsoapps.vanitygenerator.core.network.GlobalNetParams;
 import com.fatsoapps.vanitygenerator.core.query.Query;
+import com.fatsoapps.vanitygenerator.core.query.RegexQuery;
 import org.bitcoinj.core.ECKey;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 
 /**
- * Search is a Runnable that takes a @BaseSearchListener and searches for Query's defined by a user. Updates occur by
- * definitions in place of BaseSearchListener. Each Search has its own list of queries that it is searching against.
+ * Search is a Runnable that takes a @BaseSearchListener and searches for RegexQuery's (or Query's) defined by a user.
+ * Updates occur by definitions in place of BaseSearchListener. Each Search has its own list of queries that it is
+ * searching against.
  * @see com.fatsoapps.vanitygenerator.core.search.BaseSearchListener for usage.
  * @see com.fatsoapps.vanitygenerator.core.search.PoolSearch for a more multi-threaded approach.
  * <b>If this is implemented on Android, you need to extend this class and set the appropriate thread priority.</b>
@@ -18,20 +20,20 @@ public class Search implements Runnable {
 
     private BaseSearchListener listener;
     private GlobalNetParams netParams;
-    private ArrayList<Query> queries;
+    private ArrayList<RegexQuery> queries;
     private long updateAmount = 1000;
     private long generated;
     private long startTime;
 
-    public Search(BaseSearchListener listener, GlobalNetParams netParams, long updateAmount, Query... queries) {
+    public Search(BaseSearchListener listener, GlobalNetParams netParams, long updateAmount, RegexQuery... queries) {
         this(listener, netParams, queries);
         this.updateAmount = updateAmount;
     }
 
-    public Search(BaseSearchListener listener, GlobalNetParams netParams, Query... queries) {
+    public Search(BaseSearchListener listener, GlobalNetParams netParams, RegexQuery... queries) {
         this.listener = listener;
         this.netParams = netParams;
-        this.queries = new ArrayList<Query>(Arrays.asList(queries));
+        this.queries = new ArrayList<RegexQuery>(Arrays.asList(queries));
         startTime = System.currentTimeMillis();
     }
 
@@ -40,7 +42,7 @@ public class Search implements Runnable {
         while (!Thread.interrupted() && queries.size() > 0) {
             key = new ECKey();
             generated++;
-            for (Query query: queries) {
+            for (RegexQuery query: queries) {
                 if (query.matches(key, netParams)) {
                     if (listener != null) {
                         listener.onAddressFound(key, generated, getGeneratedPerSecond(), query.isCompressed());
