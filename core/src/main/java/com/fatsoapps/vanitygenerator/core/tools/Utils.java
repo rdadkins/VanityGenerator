d196d69c5b03dee5f7b5c9900aa36e9b322de84d
Rider Adkins <rdadkins@users.noreply.github.com>
2015-11-28 21:30:03 -0500

    Updated getOdds method and documentation in Utils

--- core/src/main/java/com/fatsoapps/vanitygenerator/core/tools/Utils.java
+++ core/src/main/java/com/fatsoapps/vanitygenerator/core/tools/Utils.java
@@ -10,44 +10,39 @@ public class Utils {
     public static final int BASE = 58;
     public static final char[] NON_REPEAT_CHARS = new char[]{'i', 'L', 'o'};
 
+    public static void main(String[] args) {
+        System.out.println(getOdds("abc", false, false));
+    }
+
     /**
      * Calculates the estimated number of address needed to be generated to obtain an address that contains query.
      * Cases are as follows:
      *      Case    |   Placement   |   Outcome
-     *      Match   |   Begins      |   58 ^ (length - 1)
-     *      Match   |   Contains    |   58 ^ (length)
+     *      Match   |   Begins      |   58 ^ (length)
+     *      Match   |   Contains    |   58 ^ (length) / 32 (semi-average length not including prefix of an address)
      *      Ignore  |   Contains    |   For each character (multiply):
      *                                      If it is in NON_REPEAT_CHARS: 58
      *                                      Else: 29 (58 / 2)
+     *                                      Finally divide by 32
      *      Ignore  |   Begins      |   Same as Ignore Contains except you divide by 29 or 58 depending on uniqueness.
      * @return estimated number of addresses to be generated.
      */
     public static BigInteger getOdds(String query, boolean begins, boolean match) {
         BigInteger amount;
         if (match) {
-            amount = new BigInteger(String.valueOf(BASE));
-            if (begins) {
-                amount = amount.pow(query.length() - 1);
-            } else {
-                amount = amount.pow(query.length());
-            }
+            amount = new BigInteger(String.valueOf(BASE)).pow(query.length());
         } else {
-            long estimated = 1;
+            amount = BigInteger.ONE;
             for (char c: query.toCharArray()) {
                 if (containsUniqueChar(c)) {
-                    estimated *= BASE; // Unique character which is 1 / 58
+                    amount = amount.multiply(BigInteger.valueOf(BASE)); // Unique character which is 1 / 58
                 } else {
-                    estimated *= (BASE / 2); // Ex: A or a | B or b etc.
+                    amount = amount.multiply(BigInteger.valueOf(BASE).divide(BigInteger.valueOf(2))); // Ex: A or a | B or b etc.
                 }
             }
-            if (begins) {
-                if (containsUniqueChar(query.charAt(0))) {
-                    estimated /= 58;
-                } else {
-                    estimated /= 29;
-                }
-            }
-            amount = BigInteger.valueOf(estimated);
+        }
+        if (!begins) {
+            amount = amount.divide(BigInteger.valueOf(32));
         }
         return amount;
     }
