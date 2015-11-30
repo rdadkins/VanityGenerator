04b731721fbb072851127760270db8cbc07dee4c
Rider Adkins <rdadkins@users.noreply.github.com>
2015-11-30 13:29:49 -0500

    Changed type of exception from Exception to RuntimeException of IllegalDecimalVersionException

--- core/src/main/java/com/fatsoapps/vanitygenerator/core/network/IllegalDecimalVersionException.java
+++ core/src/main/java/com/fatsoapps/vanitygenerator/core/network/IllegalDecimalVersionException.java
@@ -1,6 +1,6 @@
 package com.fatsoapps.vanitygenerator.core.network;
 
-public class IllegalDecimalVersionException extends Exception {
+public class IllegalDecimalVersionException extends RuntimeException {
 
     public IllegalDecimalVersionException(int version) {
         super("Illegal Decimal Version: " + version + ". Must be in range [0, 255].");
