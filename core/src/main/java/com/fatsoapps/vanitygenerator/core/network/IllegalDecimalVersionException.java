9a009ff20b0e1998ef13e1b9387303015c43c803
Rider Adkins <rideradkins@gmail.com>
2015-12-06 16:20:32 -0500

    Added a small description for IllegalDecimalVersionException

--- core/src/main/java/com/fatsoapps/vanitygenerator/core/network/IllegalDecimalVersionException.java
+++ core/src/main/java/com/fatsoapps/vanitygenerator/core/network/IllegalDecimalVersionException.java
@@ -1,5 +1,10 @@
 package com.fatsoapps.vanitygenerator.core.network;
 
+/**
+ * IllegalDecimalVersionException is raised when an integer is being checked if it is in a valid range [0, 255] for
+ * creating addresses. The Bitcoin protocol only accepts the closed range of [0, 255], so, anything outside of the range
+ * will throw an exception later down the line when trying to convert an ECKey to an Address.
+ */
 public class IllegalDecimalVersionException extends RuntimeException {
 
     public IllegalDecimalVersionException(int version) {
