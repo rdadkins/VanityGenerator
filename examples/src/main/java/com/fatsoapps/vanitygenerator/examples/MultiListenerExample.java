3cd4e5c5a159f6f85a7759085476dd1cea7c8ba4
Rider Adkins <rideradkins@gmail.com>
2015-12-08 19:45:30 -0500

    Added a new example MultiListenerExample which shows how to register multiple listeners to a PoolSearch

---
+++ examples/src/main/java/com/fatsoapps/vanitygenerator/examples/MultiListenerExample.java
@@ -0,0 +1,48 @@
+package com.fatsoapps.vanitygenerator.examples;
+
+import com.fatsoapps.vanitygenerator.core.network.GlobalNetParams;
+import com.fatsoapps.vanitygenerator.core.network.Network;
+import com.fatsoapps.vanitygenerator.core.query.QueryPool;
+import com.fatsoapps.vanitygenerator.core.query.RegexQuery;
+import com.fatsoapps.vanitygenerator.core.search.BaseSearchListener;
+import com.fatsoapps.vanitygenerator.core.search.PoolSearch;
+import org.bitcoinj.core.ECKey;
+
+import java.util.regex.Pattern;
+
+/**
+ * This is an example of how to use multiple BaseSearchListeners in PoolSearch
+ */
+public class MultiListenerExample {
+
+    public static void main(String[] args) {
+        new MultiListenerExample().startExample();
+    }
+
+    public void startExample() {
+        QueryPool pool = QueryPool.getInstance(Network.BITCOIN, true);
+        RegexQuery query = new RegexQuery(Pattern.compile(".*test.*"), true);
+        pool.addQuery(query);
+        PoolSearch search = new PoolSearch(getListener(0), pool, GlobalNetParams.get(pool.getNetwork()));
+        search.registerListener(getListener(1));
+        new Thread(search).start();
+    }
+
+    public BaseSearchListener getListener(final int id) {
+        return new BaseSearchListener() {
+
+            public void onAddressFound(ECKey key, GlobalNetParams netParams, long amountGenerated, long speedPerSecond, boolean isCompressed) {
+                System.out.printf("%d:[onAddressFound] %s%n", id, key.toAddress(netParams));
+            }
+
+            public void updateBurstGenerated(long totalGenerated, long burstGenerated, long speedPerSecond) {
+                System.out.printf("%d:[updateBurstGenerated] %d%n", id, burstGenerated);
+            }
+
+            public void onTaskCompleted(long totalGenerated, long speedPerSecond) {
+                System.out.printf("%d:[onTaskComplete] %d%n", id, totalGenerated);
+            }
+        };
+    }
+
+}
