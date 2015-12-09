8ee2d250ae99589b83c215e137c35e623b9041e6
Rider Adkins <rideradkins@gmail.com>
2015-12-08 19:41:05 -0500

    PoolSearch now allows multiple BaseSearchListeners via registerListener()

--- core/src/main/java/com/fatsoapps/vanitygenerator/core/search/PoolSearch.java
+++ core/src/main/java/com/fatsoapps/vanitygenerator/core/search/PoolSearch.java
@@ -5,29 +5,32 @@ import com.fatsoapps.vanitygenerator.core.query.QueryPool;
 import com.fatsoapps.vanitygenerator.core.query.RegexQuery;
 import org.bitcoinj.core.ECKey;
 
+import java.util.ArrayList;
+
 /**
+ * <b>If this is implemented in Android, you need extend and set the appropriate thread priority.</b><br/>
+ *
  * PoolSearch is a Runnable that takes a @BaseSearchListener and searches for Query's defined in a QueryPool. The user
  * can define multiple PoolSearch's in separate threads to achieve a more multi-threaded approach of searching.
  * @see com.fatsoapps.vanitygenerator.core.search.Search for a single threaded instance of searching.
- * <b>If this is implemented in Android, you need to set the appropriate thread priority.</b>
  */
 public class PoolSearch implements Runnable {
 
-    private BaseSearchListener listener;
+    private final static ArrayList<BaseSearchListener> listeners = new ArrayList<BaseSearchListener>();
     private GlobalNetParams netParams;
     private QueryPool pool;
     private long updateAmount = 1000;
     private static volatile long startTime = 0;
     private static volatile long generated = 0;
-    private static volatile long FINAL_GENERATED;
-    private static volatile boolean taskCompletedCalled = false;
 
     /**
      * Creates a PoolSearch thread from a listener, an existing QueryPool instance, and an existing GlobalNetParams
      * instance. Note: if the reference to this GNP changes, it WILL affect the way this thread searches for Query's.
      */
     public PoolSearch(BaseSearchListener listener, QueryPool pool, GlobalNetParams netParams) {
-        this.listener = listener;
+        if (listener != null && !listeners.contains(listener)) {
+            listeners.add(listener);
+        }
         this.pool = pool;
         this.netParams = netParams;
         if (startTime == 0) {
@@ -42,23 +45,42 @@ public class PoolSearch implements Runnable {
             key = new ECKey();
             generated++;
             if ((query = pool.matches(key, netParams)) != null) {
-                FINAL_GENERATED = generated;
-                if (listener != null) {
-                    listener.onAddressFound(key, query.getNetworkParameters(netParams), generated, getGeneratedPerSecond(), query.isCompressed());
+                long FINAL_GENERATED = generated;
+                if (listeners.size() > 0) {
+                    addressFound(key, query.getNetworkParameters(netParams), generated, getGeneratedPerSecond(), query.isCompressed());
                 }
                 if (!query.isFindUnlimited()) {
                     pool.removeQuery(query);
                     if (!pool.containsQueries()) {
-                        if (listener != null) {
-                            listener.onTaskCompleted(FINAL_GENERATED, getGeneratedPerSecond());
+                        if (listeners.size() > 0) {
+                            taskCompleted(FINAL_GENERATED, getGeneratedPerSecond());
                         }
                         startTime = 0;
                     }
                 }
             }
-            if (generated % updateAmount == 0 && listener != null) {
-                listener.updateBurstGenerated(generated, updateAmount, getGeneratedPerSecond());
+            if (generated % updateAmount == 0 && listeners.size() > 0) {
+                burstGenerated(generated, updateAmount, getGeneratedPerSecond());
             }
+        }
+        listeners.clear();
+    }
+
+    private synchronized void addressFound(ECKey key, GlobalNetParams netParams, long generated, long speed, boolean compressed) {
+        for (BaseSearchListener listener: listeners) {
+            listener.onAddressFound(key, netParams, generated, speed, compressed);
+        }
+    }
+
+    private synchronized void taskCompleted(long generated, long speed) {
+        for (BaseSearchListener listener: listeners) {
+            listener.onTaskCompleted(generated, speed);
+        }
+    }
+
+    private synchronized void burstGenerated(long generated, long burstGenerated, long speed) {
+        for (BaseSearchListener listener: listeners) {
+            listener.updateBurstGenerated(generated, burstGenerated, speed);
         }
     }
 
@@ -76,12 +98,12 @@ public class PoolSearch implements Runnable {
     }
 
     public PoolSearch registerListener(BaseSearchListener listener) {
-        this.listener = listener;
+        listeners.add(listener);
         return this;
     }
 
-    public PoolSearch unregisterListener() {
-        listener = null;
+    public PoolSearch unregisterListener(BaseSearchListener listener) {
+        listeners.remove(listener);
         return this;
     }
 
