e4a1320ab568b3d31b673a3d8580902515accee3
Rider Adkins <rideradkins@gmail.com>
2016-03-31 17:11:51 -0400

    Deprecated AndroidSearch

--- android/src/main/java/co/bitsquared/vanitygenerator/android/search/AndroidSearch.java
+++ android/src/main/java/co/bitsquared/vanitygenerator/android/search/AndroidSearch.java
@@ -9,7 +9,10 @@ import co.bitsquared.vanitygenerator.core.search.Search;
 /**
  * AndroidSearch is an extension of Search that sets the correct thread priority for searching. All other logic is held
  * within Search.
+ * @deprecated Please use AndroidPoolSearch
+ * @since v1.2.0
  */
+@Deprecated
 public class AndroidSearch extends Search {
 
     public AndroidSearch(BaseSearchListener listener, GlobalNetParams netParams, RegexQuery... queries) {
